244d5452ee6cda5ba9e28e6426d558f77c2f6c94
Henry Van Styn <vanstyn@intellitree.com>
2016-06-15 09:03:05 -0400

    Add role check code to filter out '200' unless code_200

--- lib/Rapi/Demo/LogView/Module/GridBase.pm
+++ lib/Rapi/Demo/LogView/Module/GridBase.pm
@@ -8,6 +8,25 @@ extends 'Catalyst::Plugin::RapidApp::RapidDbic::TableBase';
 
 use RapidApp::Util ':all';
 
+# The ResultSet chains through this method, if defined
+sub ResultSet {
+  my $self = shift;
+  my $Rs = shift;
+  
+  if($self->ResultSource->source_name eq 'RequestLog') {
+  
+    my $c = RapidApp->active_request_context;
+    
+    $Rs = $Rs->search_rs({ 'me.status' => { '!=' => 200 } }) if (
+      $c && $c->can('check_any_user_role') &&
+      ! $c->check_any_user_role(qw/administrator code_200/)
+    );
+  
+  }
+  
+  return $Rs
+}
+
 
 1;
 

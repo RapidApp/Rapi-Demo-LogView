943710e1d08a8d38262708aa521a92b1ac5b5fdf
Henry Van Styn <vanstyn@intellitree.com>
2016-06-14 20:20:23 -0400

    RAN: perl devel/model_DB_updater.pl --from-ddl --cfg --go

--- lib/Rapi/Demo/LogView/DB/Result/RequestLog.pm
+++ lib/Rapi/Demo/LogView/DB/Result/RequestLog.pm
@@ -17,19 +17,19 @@ __PACKAGE__->add_columns(
   "id",
   { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
   "host",
-  { data_type => "varchar", is_nullable => 1 },
+  { data_type => "varchar", is_nullable => 0, size => 15 },
   "ident",
   { data_type => "varchar", is_nullable => 1 },
   "authuser",
   { data_type => "varchar", is_nullable => 1 },
   "date",
-  { data_type => "varchar", is_nullable => 1 },
+  { data_type => "datetime", is_nullable => 0 },
   "request",
-  { data_type => "varchar", is_nullable => 1 },
+  { data_type => "varchar", is_nullable => 0 },
   "status",
-  { data_type => "varchar", is_nullable => 1 },
+  { data_type => "integer", is_nullable => 0 },
   "bytes",
-  { data_type => "varchar", is_nullable => 1 },
+  { data_type => "integer", is_nullable => 1 },
   "referer",
   { data_type => "varchar", is_nullable => 1 },
   "useragent",
@@ -38,8 +38,8 @@ __PACKAGE__->add_columns(
 __PACKAGE__->set_primary_key("id");
 
 
-# Created by DBIx::Class::Schema::Loader v0.07043 @ 2016-06-13 16:12:26
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:1O+IQN/SvPnyKL37eTmvCw
+# Created by DBIx::Class::Schema::Loader v0.07043 @ 2016-06-14 20:20:09
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:b0yVt9VX2VpH2ukX+aQjDQ
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
